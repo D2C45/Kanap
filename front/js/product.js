90c76249393dada3e1397e5a8ebb078dc8b0b28e
D2C45 <d2c.github@gmail.com>
2022-05-23 12:04:01 +0200

    V0.1.0: Add functions to add products to cart

--- front/js/product.js
+++ front/js/product.js
@@ -50,3 +50,92 @@ request("../js/config.json")
                 addElements(couch);
             });
     });
+
+// ---------------------------------------------------------------
+
+/**
+ * Sauvegarde le panier
+ * @param {Object} cart le panier
+ */
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+/**
+ * Récupère le panier
+ * @returns {JSON | Array} le panier ou un tableau vide si le panier n'existe pas
+ */
+function getCart() {
+    let cart = localStorage.getItem("cart");
+    if (cart == null) {
+        return [];
+    } else {
+        return JSON.parse(cart);
+    }
+}
+
+/**
+ * Recherche si le produit est déjà présent dans le panier avec le même id et la même couleur
+ * @param {Object} product le produit
+ * @param {JSON} cart le panier
+ * @returns {Object | undefined} le produit trouvé ou undefined si non trouvé
+ */
+function findProductIntoCart (product, cart) {
+    let found = undefined;
+    for (let article of cart) {
+        if (article.id == product.id && article.color == product.color) {
+            found = article;
+            break;
+        }
+    }
+    return found;
+}
+
+/**
+ * Crée l'objet produit avec les caractéristiques (id, quantité et couleur) récupérées sur la page courante
+ * @returns {object}
+ */
+function createProductObject() {
+    let product = {
+        id: currentId,
+        quantity: eval(document.getElementById('quantity').value, 10),
+        color: document.getElementById('colors').value
+    }
+    return product;
+}
+
+/**
+ * Ajoute le produit au panier ou incrémente la quantité si le produit est déjà présent dans le panier
+ * @param {object} product
+ */
+function addToCart(product) {
+    let cart = getCart();
+    let found = findProductIntoCart(product, cart);
+    if (found != undefined) {
+        found.quantity += product.quantity;
+    } else {
+        cart.push(product);
+    }
+    saveCart(cart);
+}
+
+/**
+ * Vérifie que les champs couleur et quantité soit correctement renseignés
+ * Renvoie une alerte si mal renseignés
+ * Ajoute au panier si bien renseignés
+ */
+function checkInputAndAddToCart() {
+    let product = createProductObject();
+    if (product.color == '') {
+        window.alert("Veuillez choisir une couleur");
+    } else {
+        if (product.quantity < 1 || product.quantity > 100 || !Number.isInteger(product.quantity)) {
+            window.alert("Veuillez choisir une quantité valable entre 1 et 100");
+        } else {
+            addToCart(product);
+        }
+    }
+}
+
+// Au click sur le bouton "Ajouter au panier", vérifie les champs et ajoute au panier
+document.getElementById('addToCart').addEventListener('click', checkInputAndAddToCart);
